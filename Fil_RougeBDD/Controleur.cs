eb7a94162557551ca1d2749178090e15ce21bcdc
Benoit <benoit.feurgard@gmail.com>
2018-05-11 22:49:21 +0200

    creation partielle des classes

---
+++ Fil_RougeBDD/Controleur.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fil_RougeBDD
+{
+    class Controleur
+    {
+        private int IdCO;
+        private string nomCO;
+        private string prenomCO;
+        public Controleur (int id,string nom,string prenom)
+        {
+            this.IdCO = id;
+            this.nomCO = nom;
+            this.prenomCO = prenom;
+        }
+        public int IDCO
+        {
+            get { return this.IdCO; }
+        }
+        public string NomCO
+        {
+            get { return this.nomCO; }
+        }
+        public string PrenomCO
+        {
+            get { return this.prenomCO; }
+        }
+    }
+}
---
+++ Fil_RougeBDD/Theme.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fil_RougeBDD
+{
+    class Theme
+    {
+        private string nomT;
+        private int arrondissementT;
+        public Theme (string nom, int arr)
+        {
+            this.nomT= nom;
+            this.arrondissementT = arr;
+        }
+        public string NomT
+        {
+            get { return this.nomT; }
+        }
+        public int ArrondissementT
+        {
+            get { return this.arrondissementT; }
+        }
+    }
+}
