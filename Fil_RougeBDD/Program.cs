de73bf69b098c6010cf1579858fe59c941aaf4d8
Thibaut <thibaut.tucoulou@sfr.fr>
2018-05-16 19:08:00 +0200

    Toutes les modifs sont faites, je suis enfin la où je m'étais arreté

--- Fil_RougeBDD/Program.cs
+++ Fil_RougeBDD/Program.cs
@@ -29,8 +29,8 @@ namespace Fil_RougeBDD
                 if (J3(appartements_valides))
                 {
                     Message2(connectionString,mon_id_client,liste_info_sejour,liste_info_client,id_sejour_choisi,voiture_dispo,appartements_valides);
-                    //Message3(sejour_client, un_client, Appartements_valides, listeReserver,listeV,voiture_dispo);
-                    //Message4(sejour_client, un_client, listeT);
+                    Message3(connectionString,mon_id_client,id_sejour_choisi,appartements_valides,liste_info_sejour, voiture_dispo);
+                    Message4(mon_id_client,liste_info_client,liste_info_sejour);
                 }
                 else Console.WriteLine("Pas d'appartement disponible conforme à votre recherche.");
             }
@@ -183,7 +183,7 @@ namespace Fil_RougeBDD
             {
                 connection.Close();
                 connection.Open();
-                command.CommandText = "select r.immat from ranger r,voiture v where v.immat=r.immat and v.disponible=true ";
+                command.CommandText = "select immat from voiture where disponible=true ";
                 reader = command.ExecuteReader();
                 if(reader.Read())
                 {
@@ -229,40 +229,48 @@ namespace Fil_RougeBDD
             if (voiture != "") return true;
             else return false;
         }
-        public static bool J3(List<List<string>> Appartements_valides)
+        public static bool J3(List<List<string>> appartements_valides)
         {
-            if (Appartements_valides != null) return true;
+            if (appartements_valides != null) return true;
             else return false;
         }
         public static void Message2(string connectionString,int id_client, List<string> liste_sejour, List<string> liste_client, int id_sejour_choisi, string voiture_dispo, List<List<string>> appartements_valides)
         {
-            //listeReserver.Add(new Reserver(adherent.Num_c, sejour_selectionne.Id_sejour, "14", false, -1));
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
-            MySqlCommand command = connection.CreateCommand();
-            command.CommandText = "insert into reserver values ("+id_client+","+id_sejour_choisi+",'"+liste_sejour[0]+"',false,-1)";
             MySqlDataReader reader;
-            reader = command.ExecuteReader();Console.WriteLine("haa"); Console.ReadKey();
-
-
-
-            command.CommandText = "select ";
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "select num_c from reserver where num_c="+id_client+" and id_s="+id_sejour_choisi+" and date_r='18-01-"+liste_sejour[0]+"'";
             reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+            }
+            else
+            {
+                connection.Close();
+                connection.Open();
+                command.CommandText = "insert into reserver values (" + id_client + "," + id_sejour_choisi + ",'18-01-" + liste_sejour[0] + "',false,-1)";
+                reader = command.ExecuteReader();
+                reader.Read();
+            }
             string nom_parking = "";
             string num_place = "";
-            while (reader.Read())
+            connection.Close();
+            connection.Open();
+            command.CommandText = "select p.nom, r.num_place from ranger r, parking p where r.immat='"+voiture_dispo+"' and r.id_p=p.id_p";
+            reader = command.ExecuteReader();
+            reader.Read();
+            if (reader.Read())
             {
                 nom_parking = reader.GetString(0);
                 num_place = reader.GetString(1);
-                break;
             }
-            /*
             XmlDocument docXml = new XmlDocument();
             docXml.CreateXmlDeclaration("1.0", "UTF -8", "no");
             XmlElement racine = docXml.CreateElement("M2");
             docXml.AppendChild(racine);
             XmlElement autreBalise = docXml.CreateElement("NumeroDeSejour");
-            autreBalise.InnerText = sejour_selectionne.Id_sejour;
+            autreBalise.InnerText = id_sejour_choisi.ToString();
             racine.AppendChild(autreBalise);
             XmlElement deuxiemeBalise = docXml.CreateElement("Adherent");
             racine.AppendChild(deuxiemeBalise);
@@ -270,7 +278,7 @@ namespace Fil_RougeBDD
             baliseAdherentNom.InnerText = liste_client[0];
             deuxiemeBalise.AppendChild(baliseAdherentNom);
             XmlElement baliseAdherentnum = docXml.CreateElement("Numero");
-            baliseAdherentnum.InnerText = id_client;
+            baliseAdherentnum.InnerText = id_client.ToString();
             deuxiemeBalise.AppendChild(baliseAdherentnum);
             XmlElement nomTheme = docXml.CreateElement("NomTheme");
             nomTheme.InnerText = liste_sejour[1];
@@ -290,77 +298,73 @@ namespace Fil_RougeBDD
             numeroPlace.InnerText = num_place;
             parking.AppendChild(numeroPlace);
             XmlElement immat = docXml.CreateElement("ImmatriculationVoiture");
-            immat.InnerText = voiture_selectionne.Immat;
+            immat.InnerText = voiture_dispo;
             parking.AppendChild(immat);
             XmlElement PropositionAppartement = docXml.CreateElement("PropositionsAppartements");
             racine.AppendChild(PropositionAppartement);
-
             XmlElement Proposition1 = docXml.CreateElement("Proposition1");
             PropositionAppartement.AppendChild(Proposition1);
             XmlElement Host_id1 = docXml.CreateElement("host_id");
             Proposition1.AppendChild(Host_id1);
-            Host_id1.InnerText = appartements[0][1];
+            Host_id1.InnerText = appartements_valides[0][1];
             XmlElement Proposition2 = docXml.CreateElement("Proposition2");
             PropositionAppartement.AppendChild(Proposition2);
             XmlElement Host_id2 = docXml.CreateElement("host_id");
             Proposition2.AppendChild(Host_id2);
-            Host_id2.InnerText = appartements[1][1];
+            Host_id2.InnerText = appartements_valides[1][1];
             XmlElement Proposition3 = docXml.CreateElement("Proposition3");
             PropositionAppartement.AppendChild(Proposition3);
             XmlElement Host_id3 = docXml.CreateElement("host_id");
             Proposition3.AppendChild(Host_id3);
-            Host_id3.InnerText = appartements[2][1];
+            Host_id3.InnerText = appartements_valides[2][1];
             XmlElement prix1 = docXml.CreateElement("prix");
             Proposition1.AppendChild(prix1);
-            prix1.InnerText = appartements[0][19];
+            prix1.InnerText = appartements_valides[0][19];
             XmlElement prix2 = docXml.CreateElement("prix");
             Proposition2.AppendChild(prix2);
-            prix2.InnerText = appartements[1][19];
+            prix2.InnerText = appartements_valides[1][19];
             XmlElement prix3 = docXml.CreateElement("prix");
             Proposition3.AppendChild(prix3);
-            prix3.InnerText = appartements[2][19];
-            docXml.Save("M2.xml");*/
+            prix3.InnerText = appartements_valides[2][19];
+            docXml.Save("M2.xml");
         }
-        
-        
-
-
-       /*
-        public static void Message3(Sejour sejour_selectionne,Client adherent, List<List<string>> appartements, List<Reserver> listeReserver, List<Voiture>listeV, Ranger voiture_dispo)
+        public static void Message3(string connectionString, int mon_id_client, int id_sejour_choisi, List<List<string>> appartements_valides, List<string> liste_info_sejour, string voiture_dispo)
         {
             XmlDocument docXml = new XmlDocument();
             docXml.CreateXmlDeclaration("1.0", "UTF -8", "no");
             XmlElement racine = docXml.CreateElement("M3");
             docXml.AppendChild(racine);
             XmlElement autreBalise = docXml.CreateElement("NumeroDeSejour");
-            autreBalise.InnerText = sejour_selectionne.Id_sejour;
+            autreBalise.InnerText = id_sejour_choisi.ToString();
             racine.AppendChild(autreBalise);
             XmlElement cinquiemeBalise = docXml.CreateElement("InfoValidation");
             cinquiemeBalise.InnerText = "Sejour validé";
             racine.AppendChild(cinquiemeBalise);
             XmlElement reference_appart = docXml.CreateElement("Reference_appartement_choisi");
-            reference_appart.InnerText =appartements[0][1] ;
+            reference_appart.InnerText = appartements_valides[0][1];
             racine.AppendChild(reference_appart);
             docXml.Save("M3.xml");
 
-            for (int i=0;i< listeReserver.Count(); i++)
-            {
-                if (listeReserver[i].Num_c == adherent.Num_c && listeReserver[i].Id_s == sejour_selectionne.Id_sejour && listeReserver[i].Date_r == "14") listeReserver[i].Confirme = true;
-            }
-            Voiture voiture = returnVoitureFromRanger(voiture_dispo, listeV);
-            if (voiture != null)
-            {
-                voiture.Disponible = false;
-                voiture.Motif = "reservee";
-            }
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            MySqlDataReader reader;
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "update reserver set confirme=true where id_s="+id_sejour_choisi+" and num_c="+ mon_id_client+" and date_r='18-01-"+ liste_info_sejour[0] +"'";
+            reader = command.ExecuteReader();
+
+            //connection.Close();
+            //connection.Open();
+            //command.CommandText = "update voiture set disponible=false where immat='"+voiture_dispo+"'";
+            //reader = command.ExecuteReader();
+
+            //connection.Close();
+            //connection.Open();
+            //command.CommandText = "update voiture set motif='reservée' where immat='" + voiture_dispo + "'";
+            //reader = command.ExecuteReader();
+            
         }
-        public static void Message4(Sejour sejour_selectionne, Client adherent, List<Theme> listeT)
+        public static void Message4(int mon_id_client, List<string> liste_client, List<string> liste_info_sejour)
         {
-            Theme theme_sejour = null;
-            for (int i = 0; i < listeT.Count(); i++)
-            {
-                if (sejour_selectionne.Theme_sejour == listeT[i].Id_theme) theme_sejour = listeT[i];
-            }
             XmlDocument docXml = new XmlDocument();
             docXml.CreateXmlDeclaration("1.0", "UTF -8", "no");
             XmlElement racine = docXml.CreateElement("M4");
@@ -368,23 +372,21 @@ namespace Fil_RougeBDD
             XmlElement deuxiemeBalise = docXml.CreateElement("Adherent");
             racine.AppendChild(deuxiemeBalise);
             XmlElement baliseAdherentNom = docXml.CreateElement("Nom");
-            baliseAdherentNom.InnerText = adherent.Nom;
+            baliseAdherentNom.InnerText = liste_client[0];
             deuxiemeBalise.AppendChild(baliseAdherentNom);
             XmlElement baliseAdherentnum = docXml.CreateElement("Numero");
-            baliseAdherentnum.InnerText = adherent.Num_c;
+            baliseAdherentnum.InnerText = mon_id_client.ToString();
             deuxiemeBalise.AppendChild(baliseAdherentnum);
             XmlElement nomTheme = docXml.CreateElement("NomTheme");
-            nomTheme.InnerText = theme_sejour.Nom_theme;
+            nomTheme.InnerText = liste_info_sejour[1];
             racine.AppendChild(nomTheme);
             XmlElement troisiemeBalise = docXml.CreateElement("Date");
-            troisiemeBalise.InnerText = "14";
+            troisiemeBalise.InnerText = liste_info_sejour[0];
             racine.AppendChild(troisiemeBalise);
             XmlElement cinquiemeBalise = docXml.CreateElement("InfoValidation");
             cinquiemeBalise.InnerText = "séjour impossible, veuillez choisir une autre date";
             racine.AppendChild(cinquiemeBalise);
             docXml.Save("M4.xml");
         }
-
-        */
     }
 }
